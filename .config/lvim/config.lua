ca4ae2d8da348d9355884c8b737d5257ec35dc2b
Dominik Helfenstein <dominik.helfenstein@gmx.de>
2022-11-13 20:10:15 +0100

    add hover effect and quicker Trouble with gd gr

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -22,8 +22,8 @@ vim.wo.number = true
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
--- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
@@ -31,21 +31,21 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
 
 -- Change theme settings
 -- lvim.builtin.theme.options.dim_inactive = true
@@ -67,8 +67,12 @@ lvim.builtin.which_key.mappings["g"] = {
   name = "+Git and GoTo",
   r = { "<cmd>Trouble lsp_references<cr>", "References" },
   d = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  k = { "<cmd>lvim.lsp.buf.hover()<cr>", "Definitions" },
+  k = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover" },
 }
+lvim.keys.normal_mode["K"] = "<cmd>lua vim.lsp.buf.hover()<cr>"
+lvim.keys.normal_mode["gr"] = "<cmd>TroubleToggle lsp_references<cr>"
+lvim.keys.normal_mode["gd"] = "<cmd>TroubleToggle lsp_definitions<cr>"
+lvim.keys.normal_mode["gi"] = "<cmd>TroubleToggle lsp_implementations<cr>"
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -198,10 +202,10 @@ require("flutter-tools").setup {} -- use defaults
 --   -- enable wrap mode for json files only
 --   command = "setlocal wrap",
 -- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "zsh",
+  callback = function()
+    -- let treesitter use bash highlight for zsh files as well
+    require("nvim-treesitter.highlight").attach(0, "bash")
+  end,
+})
