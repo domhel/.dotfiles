422dd103ef4b5c1bf44fa5bdf0f8dad955820fc8
Dominik Helfenstein <dominik.helfenstein@mway.io>
2023-05-02 20:30:19 +0200

    feat: skip symlink for existing dirs

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-for FILE in config/*; 
-	do ln -s $PWD/$FILE $HOME/.$FILE && echo "Symlinked $PWD/$FILE to $HOME/.$FILE";
+for FILE in config/*;
+	do if [ -d $HOME/.$FILE ]; 
+	then
+		echo "Skipping $PWD/$FILE: already exists"
+	else
+		ln -s $PWD/$FILE $HOME/.$FILE && echo "Symlinked $PWD/$FILE to $HOME/.$FILE"
+	fi
 done;
+ln $PWD/.zshrc $HOME/.zshrc && echo "Hard linked $PWD/.zshrc to $HOME/.zshrc";
