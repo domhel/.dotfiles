e6a69c669962b9e4bc6666ec052e051d2a5ac14d
Dominik Helfenstein <dominik.helfenstein@mway.io>
2023-05-03 21:50:36 +0200

    feat: lazygit & -docker

--- scripts/30-install_bin_software.sh
+++ scripts/30-install_bin_software.sh
@@ -17,7 +17,7 @@ chmod +x $HOME/bin/balenaEtcher
 wget -O $HOME/bin/SpeedCrunch.tar.bz2 https://bitbucket.org/heldercorreia/speedcrunch/downloads/SpeedCrunch-0.12-linux64.tar.bz2
 cd $HOME/bin
 tar -xf SpeedCrunch.tar.bz2
-rm SpeedCrunch.tar.bz2
+rm SpeedCrunch.tar.bz2 README.md
 
 # Insomnia HTTP client
 wget -O $HOME/bin/insomnia https://github.com/Kong/insomnia/releases/download/core%402023.2.0/Insomnia.Core-2023.2.0.AppImage
@@ -26,4 +26,16 @@ chmod +x $HOME/bin/insomnia
 # Altair GraphQL client
 wget -O $HOME/bin/altair https://github.com/altair-graphql/altair/releases/download/v5.0.24/altair_5.0.24_x86_64_linux.AppImage
 chmod +x $HOME/bin/altair
+
+# Lazygit
+wget -O $HOME/bin/lazygit.tar.gz https://github.com/jesseduffield/lazygit/releases/download/v0.38.2/lazygit_0.38.2_Linux_x86_64.tar.gz
+cd $HOME/bin
+tar -xf lazygit.tar.gz
+rm lazygit.tar.gz README.md LICENSE
+
+# Lazydocker
+wget -O $HOME/bin/lazydocker.tar.gz https://github.com/jesseduffield/lazydocker/releases/download/v0.20.0/lazydocker_0.20.0_Linux_x86_64.tar.gz
+cd $HOME/bin
+tar -xf lazydocker.tar.gz
+rm lazydocker.tar.gz README.md LICENSE
 
