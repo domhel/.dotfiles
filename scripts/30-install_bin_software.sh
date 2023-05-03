03f34d9a6a9ac0263f31dbc66dea003e2ab45b46
Dominik Helfenstein <dominik.helfenstein@mway.io>
2023-05-03 22:06:11 +0200

    feat: spyglass

--- scripts/30-install_bin_software.sh
+++ scripts/30-install_bin_software.sh
@@ -49,3 +49,7 @@ cd $HOME/bin
 tar -xf vscode.tar.gz
 rm vscode.tar.gz
 
+# Spyglass (personal search engine)
+wget -O $HOME/bin/spyglass https://github.com/spyglass-search/spyglass/releases/download/v2023.4.1/spyglass_23.4.1_amd64.AppImage
+chmod +x $HOME/bin/spyglass
+
