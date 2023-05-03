9f295a9beff84bf8fba6eb4bbfbfba0603481d75
Dominik Helfenstein <dominik.helfenstein@mway.io>
2023-05-03 21:28:26 +0200

    feat: speedcrunch

--- scripts/30-install_bin_software.sh
+++ scripts/30-install_bin_software.sh
@@ -7,8 +7,15 @@ mkdir -p $OUTPUT_FOLDER
 wget -O $OUTPUT_FOLDER/$OUTPUT_FILE https://builds.openlogic.com/downloadJDK/openlogic-openjdk/11.0.18+10/$DOWNLOADED_FILE
 cd $OUTPUT_FOLDER
 tar -xf $OUTPUT_FILE
+rm $OUTPUT_FILE
 
 # balena etcher for flashing usb sticks
 wget -O $HOME/bin/balenaEtcher https://github.com/balena-io/etcher/releases/download/v1.18.4/balenaEtcher-1.18.4-x64.AppImage
 chmod +x $HOME/bin/balenaEtcher
+
+# SpeedCrunch calculator
+wget -O $HOME/bin/SpeedCrunch.tar.bz2 https://bitbucket.org/heldercorreia/speedcrunch/downloads/SpeedCrunch-0.12-linux64.tar.bz2
+cd $HOME/bin
+tar -xf SpeedCrunch.tar.bz2
+rm SpeedCrunch.tar.bz2
 
