cc3cabbb3cb7200ec31eab19c45d401e75cb8cd0
Dominik Helfenstein <dominik.helfenstein@bluerange.io>
2023-05-13 11:28:41 +0200

    overwrite existing .configs

--- scripts/20-create_symlinks.sh
+++ scripts/20-create_symlinks.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
 for FILE in config/*;
-	do if [ -d $HOME/.$FILE ]; 
-	then
-		echo "Skipping $PWD/$FILE: already exists"
-	else
-		ln -s $PWD/$FILE $HOME/.$FILE && echo "Symlinked $PWD/$FILE to $HOME/.$FILE"
-	fi
+    rm -rf $HOME/.$FILE && ln -s $PWD/$FILE $HOME/.$FILE && echo "Symlinked $PWD/$FILE to $HOME/.$FILE"
 done;
-ln $PWD/.zshrc $HOME/.zshrc && echo "Hard linked $PWD/.zshrc to $HOME/.zshrc";
+rm $HOME/.zshrc && ln $PWD/.zshrc $HOME/.zshrc && echo "Hard linked $PWD/.zshrc to $HOME/.zshrc";
