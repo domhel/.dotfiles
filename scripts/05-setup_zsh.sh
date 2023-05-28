e0927afb40ffd900d2fae54dbd2d607d35ca2b2f
Dominik Helfenstein <dominik.helfenstein@bluerange.io>
2023-05-28 19:52:22 +0200

    feat: disable git status after cd

--- scripts/05-setup_zsh.sh
+++ scripts/05-setup_zsh.sh
@@ -1,3 +1,6 @@
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# don't run git status when cd'ing into a directory because large directories take over a second
+git config --global oh-my-zsh.hide-status 1
+
